ec4c5e738d556d9ee1405215cbe99d74e00a953a
Frenguin <104327737+Frenguin@users.noreply.github.com>
2025-03-05 23:52:12 -0600

    added container for bar graph

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,66 +1,63 @@
 import './DashboardStyles.sass'
+import { TbPigMoney } from "react-icons/tb";
+import { TbMoneybag } from "react-icons/tb";
+import { FaMoneyBillTrendUp } from "react-icons/fa6";
+import { TbZoomMoney } from "react-icons/tb";
+import { GiReceiveMoney } from "react-icons/gi";
 
 // import {useSelector} from "react-redux";
 import BRadarChart from "./graphs/RadarChart.jsx";
 
 function Dashboard (){
     // const title = useSelector((state) => state.pageTitle.pageTitle)
+    //ToDo: enter this search in google again "react get days prior to date for graph"
+
     return (
         <>
             <div className={'dash-title'}><h1>Dashboard</h1></div>
-            <div className={'row mx-0 graph-row-1 gap-4'}>
-                <div className={'col-lg-3 first-dash-row'}>
-                    <p className={'expense-title text-center mt-3'}>Expense Tracking</p>
-                    <div className={'row ms-0 me-0 mt-4'}>
-                        <p className={'text-center ex-titles'}>Balance</p>
-                        <p className={'text-center'} id={'dash-balance'}>$6000</p>
+            <div className={'row mx-0 first-row gap-4'}>
+                <div className={'col-md-2 mini-display'}>
+                    <div className={'h-50'}>
+                        <p className={'dash-currency'} id={'bal-num'}>$6000</p>
+                        <p className={'dash-currency-desc'}>Total balance for your account</p>
                     </div>
-                    <div className={'row mx-0'}>
-                        <div className={'col-6'}>
-                            <p className={'text-center ex-titles'}>Expenses</p>
-                            <p className={'text-center'} id={'dash-expense'}>-$300</p>
-                        </div>
-                        <div className={'col-6'}>
-                            <p className={'text-center ex-titles'}>Budget</p>
-                            <p className={'text-center'} id={'dash-budget'}>$2200</p>
-                        </div>
-                        <p className={'text-center ex-titles'}>Spending</p>
-                        <div className="progress px-0 mt-2" role="progressbar" aria-label="Success example" aria-valuenow="25"
-                             aria-valuemin="0" aria-valuemax="100">
-                            <div className="progress-bar bg-success" style={{width: "58%"}}>58%</div>
-                        </div>
+                    <div className={'row dash-icons h-50'}><TbPigMoney/></div>
+                </div>
+                <div className={'col-md-2 mini-display'}>
+                    <div className={'h-50'}>
+                        <p className={'dash-currency'} id={'bud-num'}>$2200</p>
+                        <p className={'dash-currency-desc'}>Total budget amount set</p>
                     </div>
+                    <div className={'row dash-icons h-50'}><TbMoneybag/></div>
                 </div>
-                <div className={'col-lg-3 first-dash-row '}>
-                  hello
+                <div className={'col-md-2 mini-display'}>
+                    <div className={'h-50'}>
+                        <p className={'dash-currency'} id={'sum-num'}>-$300</p>
+                        <p className={'dash-currency-desc'}>Current sum total of expenses </p>
+                    </div>
+                    <div className={'row dash-icons h-50'}><FaMoneyBillTrendUp/></div>
                 </div>
-                <div className={'col-lg-5 first-dash-row'}>
-                    f
+                <div className={'col-md-2 mini-display'}>
+                    <div className={'h-50'}>
+                        <p className={'dash-currency'} id={'cat-num'}>$150</p>
+                        <p className={'dash-currency-desc'}>Top category for expenses</p>
+                    </div>
+                    <div className={'row dash-icons h-50'}><TbZoomMoney/></div>
+                </div>
+                <div className={'col-md-2 mini-display'}>
+                    <div className={'h-50'}>
+                        <p className={'dash-currency'} id={'re-num'}>$1800</p>
+                        <p className={'dash-currency-desc'}>Remaining Budget</p>
+                    </div>
+                    <div className={'row dash-icons h-50'}><GiReceiveMoney/></div>
                 </div>
             </div>
-            <div className={'row mx-0 graph-row-2 gap-4'}>
-                <div className={'col-2 mini-display'}>
-                    <p className={'dash-currency'}>$6000</p>
-                    <p className={'dash-currency-desc'}>Total balance for your account</p>
-                </div>
-                <div className={'col-2 mini-display'}>
-                    <p className={'dash-currency'}>$2200</p>
-                    <p className={'dash-currency-desc'}>Total budget amount set</p>
-                </div>
-                <div className={'col-2 mini-display'}>
-                    <p className={'dash-currency'}>-$300</p>
-                    <p className={'dash-currency-desc'}>Current sum total of expenses </p>
-                </div>
-                <div className={'col-lg-2 mini-display'}>
-                    <p className={'dash-currency'}>$150</p>
-                    <p className={'dash-currency-desc'}>Top category for expenses</p>
-                </div>
-                <div className={'col-lg-2 mini-display'}>
-                    <p className={'dash-currency'}>$1800</p>
-                    <p className={'dash-currency-desc'}>Remaining Budget</p>
-                </div>
+
+            <div className={'row ms-0 me-0 mt-3 graph-row'}>
+                <div className={'col-md-10 border border-dark'}>d</div>
             </div>
-            {/*<div className={''}>{title}</div>*/}
+
+
 
         </>
     )
